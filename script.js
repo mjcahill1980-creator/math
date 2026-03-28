d2c2f366999d0c3e844b64039fd51b5bb53e6277
mjcahill1980-creator <mjcahill1980@gmail.com>
2026-03-28 12:04:04 -0400

    fixed links

--- script.js
+++ script.js
@@ -11,22 +11,7 @@ function updateSecondDropdown() {
         second.innerHTML += "<option value='Exponents.html'>Exponents</option>";
         second.innerHTML += "<option value='Expressions.html'>Expressions</option>";
         second.innerHTML += "<option value='Sci_notation.html'>Scientific Notation</option>";
-    } 
-    else if (first === "calculus") {
-        second.style.display = "inline";
-
-        second.innerHTML += "<option value=''>-- Choose a lesson --</option>";
-        second.innerHTML += "<option value=''>Coming soon</option>";
-    } 
-    else {
+    } else {
         second.style.display = "none";
-    }
-}
-
-function selectFromDropdown() {
-    const selected = document.getElementById("secondDropdown").value;
-
-    if (selected) {
-        window.location.href = selected;
     }
 }
