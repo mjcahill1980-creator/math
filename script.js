3b0e81f799abfdaddf73552dde6f66ae759ff416
mjcahill1980-creator <mjcahill1980@gmail.com>
2026-03-28 11:59:35 -0400

    fixed videos

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function updateSecondDropdown() {
 
     if (first === "algebra") {
         second.style.display = "inline";
+
         second.innerHTML += "<option value=''>-- Choose a lesson --</option>";
         second.innerHTML += "<option value='Exponents.html'>Exponents</option>";
         second.innerHTML += "<option value='Expressions.html'>Expressions</option>";
@@ -13,43 +14,19 @@ function updateSecondDropdown() {
     } 
     else if (first === "calculus") {
         second.style.display = "inline";
+
         second.innerHTML += "<option value=''>-- Choose a lesson --</option>";
-        second.innerHTML += "<option value=''>No content yet</option>";
+        second.innerHTML += "<option value=''>Coming soon</option>";
     } 
     else {
         second.style.display = "none";
     }
-
-    second.selectedIndex = 0;
-
-    // hide all videos
-    document.querySelectorAll(".video-container iframe").forEach(v => v.style.display = "none");
 }
 
 function selectFromDropdown() {
     const selected = document.getElementById("secondDropdown").value;
 
-    document.querySelectorAll(".video-container iframe").forEach(v => v.style.display = "none");
-
     if (selected) {
-        document.getElementById(selected).style.display = "block";
+        window.location.href = selected;
     }
-}
-
-function blue() {
-    document.body.style.backgroundColor = "blue";
-    document.body.style.color = "#fff";
-
-    const dropdown = document.querySelector(".dropdown2");
-    dropdown.style.backgroundColor = "white";
-    dropdown.style.color = "black";        
-}
-
-function white() {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "#000";
-
-    const dropdown = document.querySelector(".dropdown2");
-    dropdown.style.backgroundColor = "black";
-    dropdown.style.color = "white";
 }
